fc692889dbaf9090999a9db83f53f9f7dec66e55
Erx <e.a.erath@gmail.com>
2025-08-24 16:24:11 +0200

    classes for executable code & string-encoding

--- README.md
+++ README.md
@@ -9,14 +9,11 @@ inspired by, but not copied from:
 It's Javascript, but written only with a **very** samll subset of characters, most commonly `!+()[]`\
 (see: [esolangs.org/wiki/JSFuck](https://esolangs.org/wiki/JSFuck))
 
-Currently this compiler uses the characters `{}` on top of that, I plan to change this in the future.
-
 ## how can I compile my code?
 
-Features like commandline-arguments are still missing from the compiler. Currently the last line of `compiler.js` compiles and runs a [Hello, world!](https://esolangs.org/wiki/Hello,_world!)-program, just change it to your code for the time beeing
+Features like commandline-arguments are still missing from the compiler. Currently, at the end of `compiler.js` a [Hello, world!](https://esolangs.org/wiki/Hello,_world!)-program is coompiled and run, just change it to your code for the time beeing.
 
 ## future plans for the compiler
 
-- removing dependence on `{}`
 - reading/writing code from/to files (commandline-arguments)
 - including a minimizer (for the original js-code)
--- compiler.js
+++ compiler.js
@@ -1,80 +1,120 @@
-#!/usr/bin/node
-
-//#region compiler
-
-const string_generators = []
-const char_map = {}
-
-const bool = b => {
-    if (b) return "!![]"
-    else return "![]"
-}
-const int = x => {
-    if (x == 0) return "+[]"
-    if (x == 1) return "+!![]"
-    if (x < 10) return "!+[]" + "+!+[]".repeat(x-1)
-    return `+(${str(x.toString())})`
-}
-const char = c => {
-    if (!(c in char_map)) char_map[c] = new_char(c)
-    return char_map[c]
-}
-const new_char = c => {
-    let options = []
-    for (g of string_generators){
-        let txt = eval(g)
-        if (txt.includes(c)) options.push(`(${g})[${int(txt.indexOf(c))}]`)
+class code {
+    constructor(txt = ""){this.text = txt}
+    static compile(txt){return code.func_ctor().call(code.str(txt)).call()}
+    eval(){return eval(this.text)}
+    //#region primitives
+    static arr(value = new code()){return new code(`[${value.text}]`)}
+    not(){return new code(`!${this.text}`)}
+    abs(){return new code(`+${this.text}`)}
+    seal(){return new code(`(${this.text})`)}
+    append(next){return new code(`${this.text}${next.text}`)}
+    //#endregion
+    //#region composites
+    static bool(b = false){
+        if (b) return code.num(0).not()  
+        else return code.arr().not()
     }
-    if (options.length == 0) options.push(
-        `${fromCharCode}(${int(c.charCodeAt(0))})`
-    )
-    options.sort((a,b)=>a.length-b.length)
-    return options[0]
+    static num(x = 0){
+        if (x == 0) return code.arr().abs()
+        if (x == 1) return code.bool(true).abs()
+        if (x < 10) return code.sum(Array(x).fill(code.bool(true)))
+        return code.arr(code.sum(x.toString().split("").map(e=>code.arr(code.num(Number(e)))))).abs()
+    }
+    add(addend){return this.append(addend.abs())}
+    static sum(codes){
+        let out = codes[0]
+        for (let c of codes.slice(1)){
+            out = out.add(c)
+        }
+        return out
+    }
+    call(arg = new code()){return this.append(arg.seal())}
+    toStr(){return this.add(code.arr())}
+    member(identifier = new code()){return this.append(code.arr(identifier))}
+    //#endregion
+    //#region encoded-code
+    static encoder
+    static str(txt = ""){
+        if (code.encoder == undefined) {
+            code.encoder = new encoder()
+            code.encoder.init()
+        }
+        if (txt == "") return code.arr().add(code.arr())
+        return code.sum(txt.split("").map(c=>this.encoder.char(c).seal()))
+    }
+    //#endregion
+    //#region encoder-dependend code
+    ctor(){return this.member(code.str("constructor"))}
+    static func_ctor(){return code.arr().member(code.str("find")).ctor()}
+    //#endregion
 }
-const str = s => {
-    let out = []
-    for (let c of s) out.push(`(${char(c)})`)
-    return out.join("+")
+
+class encoder {
+    char(c){
+        if (!(c in this.charmap)) this.charmap[c] = this.new_char(c)
+        return this.charmap[c]
+    }
+    new_char(c){
+        let options = []
+        for (let g of this.generators){
+            let txt = g.eval()
+            if (txt.includes(c)) options.push(
+                g.seal().member(code.num(txt.indexOf(c)))
+            )
+        }
+        if (options.length == 0) options.push(
+            code.str().seal().ctor().member(code.str("fromCharCode")).call(code.num(c.charCodeAt(0)))
+        )
+        options.sort((a,b)=>a.text.length-b.text.length)
+        return options[0]
+    }
+    constructor(){
+        this.charmap = {}
+        this.generators = []
+    }
+    init(){
+        this.generators.push(
+            code.arr().member(code.arr()).toStr(), //undefi
+            code.bool(true).seal().toStr(), //tru
+            code.bool(false).seal().toStr(), //als
+            code.arr(code.bool(false)).abs().toStr(), //N
+
+        )
+        const digits = "0123456789"
+        for (let c of digits) this.generators.push(
+            code.num(digits.indexOf(c)).seal().toStr()
+        )
+        this.generators.push(
+            code.str("1e309").seal().abs().toStr(), //Iy
+            code.str("11e100").seal().abs().toStr(), //.+
+            code.arr().member(code.str("find")).toStr(), //cov (){}[]
+        )
+        this.generators.push(
+            code.str().seal().member(code.str("fontcolor")).call()
+        )
+        this.generators.push(
+            code.str("0.0000001").seal().abs().toStr(), //-
+            code.bool().seal().ctor().toStr(), //B
+            code.num().seal().ctor().toStr(), //mb
+            code.str().seal().ctor().toStr(), //Sg
+            code.arr().ctor().toStr(), //A
+            code.func_ctor().toStr(), //F
+        )
+        const lower = "abcdefghijklmnopqrstuvwxyz"
+        for (let c of lower) this.generators.push(
+            code.num(lower.indexOf(c)+10).seal().member(code.str("toString")).call(code.num(lower.indexOf(c)+11))
+        )
+        for (let c of "[]<>") this.generators.push(
+            code.func_ctor().call(code.str("return escape")).call().call(code.str(c))
+        )
+    }
 }
-const compile = code => {
-    return `${func_ctor}(${str(code)})()`
-}
 
-//#region set up string_generators
 
-string_generators.push (
-    "[][[]]+[]",
-    `(${bool(true)})+[]`,
-    `(${bool(false)})+[]`,
-    "+[![]]+[]",
+let compiled = code.compile(
+    `console.log("Hello, world!"); return 0;`
 )
-const numeric = "0123456789"
-for (c of numeric) string_generators.push(`(${int(numeric.indexOf(c))})+[]`)
-string_generators.push(
-    `+(${str("1e309")})+[]`,
-    `+(${str("11e100")})+[]`,
-    `[][${str("find")}]+[]`,
-)
-string_generators.push(
-    `([]+[])[${str("fontcolor")}]()`,
-)
-const ctor = `[${str("constructor")}]`
-const str_ctor = `([]+[])${ctor}`
-const func_ctor = `([][${str("find")}])${ctor}`
-string_generators.push(
-    `+(${str("0.0000001")})+[]`,
-    `(${int(0)})${ctor}+[]`,
-    `(${bool(false)})${ctor}+[]`,
-    `[]${ctor}+[]`,
-    `${str_ctor}+[]`,
-    `${func_ctor}+[]`,
-)
-const lower = "abcdefghijklmnopqrstuvwxyz"
-for (c of lower) string_generators.push(`(${int(lower.indexOf(c)+10)})[${str("toString")}](${int(lower.indexOf(c)+11)})`)
-for (esc of "[]<>") string_generators.push(`${func_ctor}(${str("return escape")})()(${char(esc)})`)
-const fromCharCode = `${str_ctor}[${str("fromCharCode")}]`
-
-//#endregion
-//#endregion
-
-console.log(eval(compile("console.log(\"Hello, world!\"); return 0;")))
+console.log(`Code: ${compiled.text}`)
+console.log("Output:")
+let result = eval(compiled.text)
+console.log(`Return: ${result}`)
